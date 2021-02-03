1eee7bb6e456f8f8ad97b86c2559998aa585e57e
Alizée Bellerose <misotheism@users.noreply.github.com>
2021-02-03 08:05:26 -0500

    update

--- scripts/libs/TSV.py
+++ scripts/libs/TSV.py
@@ -3,6 +3,10 @@ import csv
 
 class Writer:
     def __init__(self, data):
+        participant_id = ''
+        age = ''
+        sex = ''
+        hand = ''
         print('- Writer: init started.')
         # print(data)
         file_path = data['bids_directory'] + '/participants.tsv'
@@ -13,11 +17,17 @@ class Writer:
 
         output = []
         for line in rows:
-            participant_id, age, sex, hand = line
+            try:
+                participant_id, age, sex, hand = line
+            except ValueError:
+                try:
+                    participant_id, age, sex, hand, site_id = line
+                except ValueError:
+                    print('error: ValueError')
             output.append([participant_id, age, sex, hand, data['site_id']])
 
         with open(file_path, 'w', newline='') as f:
-            headers = ['participant_id', 'age', 'sex', 'hand', 'siteID']
+            headers = ['participant_id', 'age', 'sex', 'hand', 'SiteID']
             writer = csv.writer(f, delimiter='\t')
             writer.writerow(headers)
             writer.writerows(output)
